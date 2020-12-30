f278ed4ce99917b656540960b1eb716edb980466
Anton Paliakou <toxa95401@gmail.com>
2020-12-31 01:16:51 +0300

    Init project.

---
+++ NotificationPusher/NotificationPusher/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  NotificationPusher
+//
+//  Created by Anton Paliakou on 12/31/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
